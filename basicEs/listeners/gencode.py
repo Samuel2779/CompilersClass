a3a72032097000d4d9a705e805f2b274fec84b01
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2022-03-13 22:25:56 -0600

    Basic Assembler Hw
    Adding basic assembler and obtaining MIPS like language

---
+++ basicEs/listeners/gencode.py
@@ -0,0 +1,96 @@
+from inspect import stack
+from turtle import position
+from typing import Counter
+from antlr.marzoListener import marzoListener
+from antlr.marzoParser import marzoParser
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = Node("head")
+        self.size = 0
+ 
+    def isEmpty(self):
+        return self.size == 0
+ 
+    def peek(self):
+ 
+        if self.isEmpty():
+            raise Exception("Peeking from an empty stack")
+        return self.head.next.value
+ 
+    def push(self, value):
+        node = Node(value)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+ 
+    def pop(self):
+        if self.isEmpty():
+            raise Exception("Popping from an empty stack")
+        remove = self.head.next
+        self.head.next = self.head.next.next
+        self.size -= 1
+        return remove.value
+
+class GenCode(marzoListener):
+   
+    def __init__(self):
+        self.stack = Stack()
+        self.counter = 0
+        self.dict = {}
+
+    def enterProgram(self, ctx:marzoParser.ProgramContext):
+        print(".BeginProgram")
+
+    def exitPrimaria(self, ctx:marzoParser.PrimariaContext):
+        self.stack.push(ctx.Numero().getText())
+
+    def exitSuma(self, ctx:marzoParser.SumaContext):
+        print("ADD $" + str(self.counter) + ', ' + str(self.stack.pop()) + ", " + str(self.stack.pop()))
+        self.counter += 1
+    
+    def enterDeclaracion(self, ctx:marzoParser.DeclaracionContext):
+        self.stack.push(ctx.getTokens(marzoParser.Name)[0].getText())
+
+    def exitDeclaracion(self, ctx:marzoParser.DeclaracionContext):
+        temp = self.stack.pop()
+        if temp not in self.dict:
+            self.dict[self.counter] = temp
+            self.counter += 1
+        key_list = list(self.dict.keys())
+        val_list = list(self.dict.values())
+        pos = val_list.index(temp)
+        print('lw $' + str(key_list[pos]) + ' '  + ', 0')
+
+    
+    def enterIfConElse(self, ctx:marzoParser.IfConElseContext):
+        print('IF ' + str(ctx.getTokens(marzoParser.Name)[0].getText()) + " equal " + str(ctx.getTokens(marzoParser.Numero)[0].getText()) + ' ELSE j ')
+        #print('Testeo de valores ' + str(ctx.getToken(marzoParser.statements, 1)))
+
+
+    def exitIfConElse(self, ctx:marzoParser.IfConElseContext):
+        print('ENDIF')
+        
+
+    def enterAssign(self, ctx:marzoParser.AssignContext):
+        self.stack.push(ctx.getTokens(marzoParser.Numero)[0].getText())
+        self.stack.push(ctx.getTokens(marzoParser.Name)[0].getText())
+
+    def exitAssign(self, ctx:marzoParser.AssignContext):
+        temp = self.stack.pop()
+        if temp not in self.dict:
+            self.dict[self.counter] = temp
+            self.counter += 1
+        key_list = list(self.dict.keys())
+        val_list = list(self.dict.values())
+        pos = val_list.index(temp)
+        print('lw $' + str(key_list[pos]) + ', ' + self.stack.pop())
+        
+
+    def exitPrintstmt(self, ctx:marzoParser.PrintstmtContext):
+        print('prt ' + ctx.getTokens(marzoParser.Numero)[0].getText())
